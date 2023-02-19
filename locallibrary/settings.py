d5e5af4e9bd84942901bfcbc7f154c503b3dfb9b
Jose Manuel Ayala Wilson <jmayalaw@gmail.com>
2023-02-19 09:19:46 +0100

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -27,7 +27,7 @@ load_dotenv(BASE_DIR / '.env')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ') 
 
 # Using environment
 
